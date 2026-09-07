d07219bd053085176fa96ab0b352f5b5ce52cc56
wild-apache <70039380+wild-apache@users.noreply.github.com>
2026-09-07 22:34:44 +0530

    fix: use datetime UTC alias in indicator test

--- tests/test_indicators_types.py
+++ tests/test_indicators_types.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timezone
+from datetime import UTC, datetime
 
 from t360.domain import Candle
 from t360.market_data import compute_indicators
@@ -8,7 +8,7 @@ def test_indicator_snapshot_uses_float_values_for_decimal_candles() -> None:
     candle = Candle(
         symbol="NSE:TEST",
         timeframe="60s",
-        timestamp=datetime(2026, 1, 1, 9, 15, tzinfo=timezone.utc),
+        timestamp=datetime(2026, 1, 1, 9, 15, tzinfo=UTC),
         open="100.10",
         high="101.10",
         low="99.10",
